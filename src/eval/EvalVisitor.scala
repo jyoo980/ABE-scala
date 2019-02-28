9774a32354faf2c1f8c16971623b375c8e8d8cc9
James Yoo <jyoo980@gmail.com>
2019-02-28 13:03:04 -0800

    Added Var implementation for EvalVisitor.scala

--- src/eval/EvalVisitor.scala
+++ src/eval/EvalVisitor.scala
@@ -6,18 +6,24 @@ import visitor.Visitor
 
 class EvalVisitor extends Visitor[Value, Environment] {
 
-  override def visit(expr: Num, param: Environment): Value = {
+  override def visit(expr: Num, env: Environment): Value = {
     new NumValue(expr.num)
   }
 
-  override def visit(expr: If, param: Environment): Value = {
-    val cond = expr.pred.accept(this, param)
+  override def visit(expr: If, env: Environment): Value = {
+    val cond = expr.pred.accept(this, env)
     if (cond.toBool()) {
-      expr.thn.accept(this, param)
+      expr.thn.accept(this, env)
     } else {
-      expr.els.accept(this, param)
+      expr.els.accept(this, env)
     }
   }
 
-  override def visit(expr: Var, param: Environment) = ???
+  override def visit(expr: Var, env: Environment) = {
+    try {
+      env.lookup(expr)
+    } catch {
+      case e: Exception => throw e
+    }
+  }
 }
